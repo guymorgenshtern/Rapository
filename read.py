a3c9d65b66f79e04ac2cb39fd047100e6e88346d
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2018-10-13 09:19:50 -0400

    Put code into functions

--- read.py
+++ read.py
@@ -1,32 +1,37 @@
-read_file = open("data.txt")
+#read_file = open("data.txt")
 
-words = []
-lines = read_file.readlines()
-for line in lines:
-    word_set = line.split()
-    for word in word_set:
-        if not word in words:
-            words.append(word)
+def generate_word_list(read_file):
+    words = []
+    lines = read_file.readlines()
+    for line in lines:
+        word_set = line.split()
+        for word in word_set:
+            if not word in words:
+                words.append(word)
 
-#Add end of line identifier
-words.append("&&&")
-print(words)
+    #Add end of line identifier
+    words.append("&&&")
+    print(words)
+    read_file.close()
+    return words
+
 
 #init array
-prob = []
-for i in range(len(words)):
-    row = []
-    for j in range(len(words)):
-        row.append(0)
-    prob.append(row)
+def generate_prob_table(words, lines):
+    prob = []
+    for i in range(len(words)):
+        row = []
+        for j in range(len(words)):
+            row.append(0)
+        prob.append(row)
 
-for line in lines:
-    word_set = line.split()
-    for i in range(len(word_set)):
-        #Link word to next word in phrase
-        if not i == len(word_set) - 1:
-            prob[words.index(word_set[i])][words.index(word_set[i + 1])] += 1
-        #If it is the last word, connect to end of line character
-        else:
-            prob[words.index(word_set[i])][len(words) - 1] += 1
-print(prob)
+    for line in lines:
+        word_set = line.split()
+        for i in range(len(word_set)):
+            #Link word to next word in phrase
+            if not i == len(word_set) - 1:
+                prob[words.index(word_set[i])][words.index(word_set[i + 1])] += 1
+            #If it is the last word, connect to end of line character
+            else:
+                prob[words.index(word_set[i])][len(words) - 1] += 1
+    print(prob)
