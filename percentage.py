d110cfd8cdd05f741fd34f8a9f5746981d9388c7
Guy Morgenshtern <guymorg17@gmail.com>
2018-10-13 10:10:16 -0400

    percentage Calc

--- percentage.py
+++ percentage.py
@@ -1,1 +1,18 @@
-from . import read
+import read
+import test
+
+word_list = read.generate_word_list("data.txt")
+prob_table = read.generate_prob_table("data.txt")
+
+test.generate_text(word_list,prob_table)
+
+for i in prob_table:
+    print("a")
+    sum = 0
+    for j in i:
+        sum += j
+    print(sum)
+    if not sum == 0:
+        for j in i:
+            print((j / sum), end=" ")
+
